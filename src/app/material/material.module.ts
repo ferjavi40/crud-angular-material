f37175a33fbceecfdc177b6801b9f49e5ed09697
ferjavi40 <ferjaviramirez@gmail.com>
2021-04-30 15:36:46 +0200

    feature: navbar done

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
-import { MatSliderModule } from '@angular/material/slider';
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
 
 
 
 @NgModule({
   declarations: [],
 exports:[
-  MatSliderModule,
-  MatButtonModule
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatMenuModule
 ]
 })
 export class MaterialModule { }
