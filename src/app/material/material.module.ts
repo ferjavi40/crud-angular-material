153f00bdabf87570940d4a3afe456bf42799a706
ferjavi40 <ferjaviramirez@gmail.com>
2021-04-30 14:50:06 +0200

    fix: add material module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
 
 
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+exports:[
+  MatSliderModule,
+  MatButtonModule
+]
 })
 export class MaterialModule { }
